1acf3941444f1800f5b042308acdcc974c999451
bawlaBaman <saiyamsharma01702@gmail.com>
2026-03-03 06:53:48 +0100

    added board movement

--- board.py
+++ board.py
@@ -1,6 +1,23 @@
 from pieces import Rook, Knight, Bishop, Queen, King, Pawn
 
+class BoardMovement:
 
+    @staticmethod
+    def forward(position, color):
+        column = position[0]
+        row = int(position[1])
+
+        if color == "WHITE":
+            new_row = row - 1
+        else:
+            new_row = row + 1
+
+        if new_row < 1 or new_row > 8:
+            return position  # block movement at edge
+
+        return f"{column}{new_row}"
+    
+    
 class Board:
 
     def __init__(self):
